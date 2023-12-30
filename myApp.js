d562817211a630dfab9416fe45399ec460c97a39
raizrazer <raizrazer@gmail.com>
2023-12-30 12:06:25 +0530

    Added mongobd and mongoose.

--- myApp.js
+++ myApp.js
@@ -1,5 +1,10 @@
-require('dotenv').config();
+require("dotenv").config();
+const mongoose = require("mongoose");
 
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 let Person;
 

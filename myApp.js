3b343576d9fbef29ece2e1801811e6a5121059e2
raizrazer <raizrazer@gmail.com>
2023-12-30 13:45:30 +0530

    Create a Person model using mongoose schema.

--- myApp.js
+++ myApp.js
@@ -6,7 +6,15 @@ mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true,
 });
 
-let Person;
+const Schema = mongoose.Schema;
+
+const personSchema = new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String],
+});
+
+const Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);

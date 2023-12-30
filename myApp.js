83b34e8b4056d79745b4fbcf9a0adfa370c70d27
raizrazer <raizrazer@gmail.com>
2023-12-30 16:39:36 +0530

    Added findByIdAndRemove()

--- myApp.js
+++ myApp.js
@@ -103,7 +103,10 @@ const findAndUpdate = (personName, done) => {
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove({ _id: personId }, (err, updateDoc) => {
+    if (err) return console.log(err);
+    done(null, updatedDoc);
+  });
 };
 
 const removeManyPeople = (done) => {

307d764984d48d32473614a4a4e4ca1f85c7175f
raizrazer <raizrazer@gmail.com>
2023-12-30 16:26:39 +0530

    Added findOneAndUpdate().

--- myApp.js
+++ myApp.js
@@ -91,8 +91,15 @@ const findEditThenSave = (personId, done) => {
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
+  Person.findOneAndUpdate(
+    { name: personName },
+    { age: ageToSet },
+    { new: true },
+    (err, updatedDoc) => {
+      if (err) return console.log(err);
+      done(null, updatedDoc);
+    }
+  );
 };
 
 const removeById = (personId, done) => {
